56e64f9a7a9d20f98cb6c5fe277fdbe3921d0eaa
FedericoDellAquila <federico.dellaquila1993@gmail.com>
2025-02-18 23:36:00 +0100

    Cleaned main branch

--- src/assets/index.ts
+++
@@ -1,7 +0,0 @@
-import logo from "./logo.png";
-import user from "./user.jpg";
-import work1 from "./work-1.jpg";
-import work2 from "./work-2.jpg";
-import work3 from "./work-3.jpg";
-
-export {logo, user, work1, work2, work3};
